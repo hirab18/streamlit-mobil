6a6384a142b29d1dc18dcaa2693384b3b74b48b0
hirab18 <132433030+hirab18@users.noreply.github.com>
2024-12-01 19:57:16 +0800

    estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -4,7 +4,7 @@ import streamlit as st
 model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
 
-st.title('Prediksi Harga Mobil Bekas')
+st.title('Prediksi Harga Mobil BMW Bekas')
 
 year = st.number_input('Input Tahun Mobil')
 mileage = st.number_input('Input Km Mobil')
